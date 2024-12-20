8b30afb5aa87fbd57d9af709dd0b1deefe2d7a40
vinodkumar679 <vinodkmp12345@gmail.com>
2024-12-20 12:01:37 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    strings:["frentend developer","youTuber","web developer"]
+        typeSpeed: 100,
+        backSpeed: 100,
+        backdelay: 1000,
+        loop:true
+    
+});
